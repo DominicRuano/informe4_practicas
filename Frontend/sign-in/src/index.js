427bb1873e74a2e4bdfc860f5177a8a63b11cd8f
DominicRuano <Dojuparupe1975@gmail.com>
2023-09-16 00:35:45 -0600

    add crear usuario

---
+++ Frontend/sign-in/src/index.js
@@ -0,0 +1,7 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RegistroUsuario from './RegistroUsuario';
+import './RegistroUsuario.css'; // Importa los estilos CSS después del componente
+
+ReactDOM.render(<RegistroUsuario />, document.getElementById('root'));
